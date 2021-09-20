5e853c9b997adc79a974a738520f2bb3bb7f8980
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-09-20 12:43:41 -0300

    viewcode setup and loginScreen

---
+++ Message/Message/Login/Screen/LoginScreen.swift
@@ -0,0 +1,207 @@
+//
+//  LoginScreen.swift
+//  Message
+//
+//  Created by Paulo Rodrigues on 20/09/21.
+//
+
+import UIKit
+
+protocol LoginScreenProtocol: AnyObject {
+    func actionLoginButton()
+    func actionRegisterButton()
+}
+
+class LoginScreen: UIView {
+    
+    private weak var delegate: LoginScreenProtocol?
+    
+    func delegate(delegate: LoginScreenProtocol) {
+        self.delegate = delegate
+    }
+    
+    lazy var loginLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.text = "Login"
+        
+        return label
+    }()
+    
+    lazy var logoAppImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField.autocorrectionType = .no
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .emailAddress
+        textField.placeholder = "Digite seu e-mail:"
+        textField.textColor = .darkGray
+        
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField.autocorrectionType = .no
+        textField.backgroundColor = .white
+        textField.borderStyle = .roundedRect
+        textField.keyboardType = .default
+        textField.isSecureTextEntry = true
+        textField.placeholder = "Digite sua senha:"
+        textField.textColor = .darkGray
+        
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.setTitle("Logar", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 7.5
+        button.backgroundColor = UIColor(red: 3 / 255, green: 58 / 255, blue: 51 / 255, alpha: 1.0)
+        button.addTarget(self, action: #selector(self.tappedLoginButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var registerButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.setTitle("Não tem conta? Cadastre-se", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.white, for: .normal)
+    
+        button.addTarget(self, action: #selector(self.tappedRegisterButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.configBackground()
+        self.configSuperview()
+        self.setupConstraints()
+        self.checkButtonEnabled(false)
+    }
+    
+    private func configBackground() {
+        self.backgroundColor = UIColor(red: 24 / 255, green: 117 / 255, blue: 104 / 255, alpha: 1.0)
+    }
+    
+    private func configSuperview() {
+        self.addSubview(self.loginLabel)
+        self.addSubview(self.logoAppImageView)
+        self.addSubview(self.emailTextField)
+        self.addSubview(self.passwordTextField)
+        self.addSubview(self.loginButton)
+        self.addSubview(self.registerButton)
+    }
+    
+    public func configTextFieldDelegate(delegate: UITextFieldDelegate) {
+        self.emailTextField.delegate = delegate
+        self.passwordTextField.delegate = delegate
+    }
+    
+    @objc private func tappedLoginButton() {
+        self.delegate?.actionLoginButton()
+    }
+    
+    @objc private func tappedRegisterButton() {
+        self.delegate?.actionRegisterButton()
+    }
+    
+    public func validateTextFields() {
+        let email: String = self.emailTextField.text ?? ""
+        let password: String = self.passwordTextField.text ?? ""
+        
+        if !email.isEmpty && !password.isEmpty {
+            self.checkButtonEnabled(true)
+        } else {
+            self.checkButtonEnabled(false)
+        }
+    }
+    
+    private func checkButtonEnabled(_ isButtonEnabled: Bool) {
+        if isButtonEnabled {
+            self.loginButton.setTitleColor(.white, for: .normal)
+            self.loginButton.isEnabled = true
+        } else {
+            self.loginButton.setTitleColor(.lightGray, for: .normal)
+            self.loginButton.isEnabled = false
+        }
+    }
+    
+    public func getEmail() -> String {
+        return self.emailTextField.text ?? ""
+    }
+    
+    public func getPassword() -> String {
+        return self.passwordTextField.text ?? ""
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            self.loginLabel.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.loginLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            
+            
+            self.logoAppImageView.topAnchor.constraint(equalTo: self.loginLabel.bottomAnchor, constant: 20),
+            self.logoAppImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 60),
+            self.logoAppImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -60),
+            self.logoAppImageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            
+            self.emailTextField.topAnchor.constraint(equalTo: self.logoAppImageView.bottomAnchor, constant: 20),
+            self.emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.emailTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            self.emailTextField.heightAnchor.constraint(equalToConstant: 45),
+            
+            
+            self.passwordTextField.topAnchor.constraint(equalTo: self.emailTextField.bottomAnchor, constant: 15),
+            self.passwordTextField.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.passwordTextField.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.passwordTextField.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+            
+            
+            self.loginButton.topAnchor.constraint(equalTo: self.passwordTextField.bottomAnchor, constant: 15),
+            self.loginButton.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.loginButton.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.loginButton.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+            
+            
+            self.registerButton.topAnchor.constraint(equalTo: self.loginButton.bottomAnchor, constant: 15),
+            self.registerButton.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.registerButton.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.registerButton.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+        ])
+    }
+    
+}
---
+++ Message/Message/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Message
+//
+//  Created by Paulo Rodrigues on 20/09/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    var loginScreen: LoginScreen?
+    
+    override func loadView() {
+        self.loginScreen = LoginScreen()
+        
+        self.view = self.loginScreen
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.loginScreen?.delegate(delegate: self)
+        self.loginScreen?.configTextFieldDelegate(delegate: self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
+    
+}
+
+extension LoginViewController: LoginScreenProtocol {
+    
+    func actionLoginButton() {
+        print("actionLoginButton")
+    }
+    
+    func actionRegisterButton() {
+        print("actionRegisterButton")
+    }
+    
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.loginScreen?.validateTextFields()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == self.loginScreen?.emailTextField {
+            self.loginScreen?.passwordTextField.becomeFirstResponder()
+        } else {
+            self.loginScreen?.passwordTextField.resignFirstResponder()
+        }
+        
+        return textField.resignFirstResponder()
+    }
+    
+}
+
