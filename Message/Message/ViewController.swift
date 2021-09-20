fd08632e68e2ba10f71399107bbccc8ea6cb536e
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-09-20 10:46:44 -0300

    init project

---
+++ Message/Message/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Message
+//
+//  Created by Paulo Rodrigues on 20/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
