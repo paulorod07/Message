dacba8bb30e28db46858d7588f97dff193b505fe
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-09-21 15:23:00 -0300

    CustomFont

---
+++ Message/Message/Utils/CustomFont.swift
@@ -0,0 +1,12 @@
+//
+//  CustomFont.swift
+//  Message
+//
+//  Created by Paulo Rodrigues on 21/09/21.
+//
+
+struct CustomFont {
+    static let poppinsMedium = "Poppins-Medium"
+    static let poppinsBold = "Poppins-Bold"
+    static let poppinsSemiBold = "Poppins-SemiBold"
+}
